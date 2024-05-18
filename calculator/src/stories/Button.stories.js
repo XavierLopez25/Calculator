c2092abdf91b745cec971e00afbdc41257829700
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-18 16:07:07 -0600

    feat: added storybook for Button component

--- calculator/src/stories/Button.stories.js
+++ calculator/src/stories/Button.stories.js
@@ -1,48 +1,60 @@
 import { fn } from '@storybook/test';
-import { Button } from './Button';
+import Button from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
+    buttonText: { control: 'text' },
+    className: { control: 'text' },
+    disabled: { control: 'boolean' },
     backgroundColor: { control: 'color' },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/react/essentials/actions#action-args
   args: { onClick: fn() },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
-    label: 'Button',
+    buttonText: 'Primary Button',
+    className: 'button button--primary',
   },
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
+    buttonText: 'Secondary Button',
+    className: 'button button--secondary',
   },
 };
 
 export const Large = {
   args: {
-    size: 'large',
-    label: 'Button',
+    buttonText: 'Large Button',
+    className: 'button button--primary button--large',
   },
 };
 
 export const Small = {
   args: {
-    size: 'small',
-    label: 'Button',
+    buttonText: 'Small Button',
+    className: 'button button--primary button--small',
+  },
+};
+
+export const Disabled = {
+  args: {
+    buttonText: 'Disabled Button',
+    className: 'button button--primary button--disabled',
+    disabled: true,
   },
 };
