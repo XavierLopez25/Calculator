84e1f74decb5b52fe272252e3f10dcb2c3df2e24
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-18 14:26:07 -0600

    feat: added Button component

--- calculator/src/components/Button/index.jsx
+++ calculator/src/components/Button/index.jsx
@@ -1,13 +1,19 @@
+// src/Button.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ buttonText, onClick }) => {
-  return <button onClick={onClick}>{buttonText}</button>;
+const Button = ({ buttonText, onClick, className }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {buttonText}
+    </button>
+  );
 };
 
 Button.propTypes = {
   buttonText: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
 };
 
 export default Button;
