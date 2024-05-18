5eb31378dbbb5afdc80a149323479f12a7f7d01d
XavierLopez25 <lop22716@uvg.edu.gt>
2024-05-18 15:13:21 -0600

    fix: changed import for Calculator component

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Calculator from './pages/Calculator';
+import Calculator from './components/Calculator';
 
 export default function App() {
   return (
